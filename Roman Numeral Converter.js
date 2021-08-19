ad8dc89c4f454f73b1d3ab9f5857ef12dca1fed9
milkiway7 <62120570+milkiway7@users.noreply.github.com>
2021-08-19 10:49:33 +0200

    Add files via upload

---
+++ Roman Numeral Converter.js
@@ -0,0 +1,29 @@
+function convertToRoman(num) {
+    const roman = {
+      1000:"M",
+      900:"CM",
+      500:"D",
+      400:"CD",
+      100:"C",
+      90:"XC",
+      50:"L",
+      40:"XL",
+      10:"X",
+      9:"IX",
+      5:"V",
+      4:"IV",
+      1:"I"
+    };
+  
+    let romanResult='';
+    let decimalNumbers = Object.keys(roman).reverse();
+    console.log(decimalNumbers);
+  
+    decimalNumbers.forEach(val =>{
+      while(val<=num){
+        romanResult += roman[val];
+        num -= val;
+      }
+    });
+    return romanResult;
+  }
